061488609a349a9e28e146241ee8dffee40932c0
Talon Kettuso <105325988+TalonFox@users.noreply.github.com>
2024-05-24 22:44:12 -0500

    oops

--- src/main/kotlin/sh/talonfloof/draco_std/transformation/AccessWidenerTransformer.kt
+++ src/main/kotlin/sh/talonfloof/draco_std/transformation/AccessWidenerTransformer.kt
@@ -8,11 +8,11 @@ import sh.talonfloof.dracoloader.transform.IDracoTransformer
 
 class AccessWidenerTransformer : IDracoTransformer {
     override fun transform(loader: ClassLoader, className: String, originalClassData: ByteArray?): ByteArray? {
-        if(className == "net.minecraft.world.level.block.entity.BlockEntityType") {
+        if(className == "net.minecraft.world.level.block.entity.BlockEntityType\$BlockEntitySupplier") {
             val reader = ClassReader(originalClassData)
             val node = ClassNode()
             reader.accept(node, 0)
-            node.innerClasses.find { it.name == "net/minecraft/world/level/block/entity/BlockEntityType\$BlockEntitySupplier" }!!.access = Opcodes.ACC_PUBLIC
+            node.access = Opcodes.ACC_ABSTRACT or Opcodes.ACC_INTERFACE or Opcodes.ACC_PUBLIC
             return ClassWriter(reader, ClassWriter.COMPUTE_FRAMES).also { node.accept(it) }.toByteArray()
         }
         return originalClassData
